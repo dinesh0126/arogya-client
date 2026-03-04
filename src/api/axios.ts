1d1c8dbf7fa0df3a7bb1d01b563380457f0be649
Dinesh <dineshgarig@gmail.com>
2026-03-04 11:52:07 +0530

    baseUrl update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,10 +2,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://aarogya-first-api.vercel.app/api",
+  baseURL: "https://aarogya-first-api.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
+    
 });
 
 api.interceptors.request.use((config) => {
